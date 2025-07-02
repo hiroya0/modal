b6f9d68b765b2e5af4eee14500ce1ab67846219a
hiroya0 <2000shigehiro0401@au.com>
2025-07-02 15:40:28 +0900

    モーダルを閉じる

--- modal.js
+++ modal.js
@@ -1,9 +1,13 @@
 const open = document.getElementById('openModalBtn');
 const myModal = document.getElementById('myModal');
-const close = document.getElementById('colseModalBtn');
+const close = document.getElementById('closeModalBtn');
 
 open.addEventListener('click', function(){
     myModal.style.display = 'block';
+});
+
+close.addEventListener('click',function(){
+    myModal.style.display = 'none';
 });
 
 //#myModal {
