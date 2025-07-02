bd6d27f3150dfc4660f652337f28152bd68eebe7
hiroya0 <2000shigehiro0401@au.com>
2025-07-02 14:54:05 +0900

    モーダルを出す流れを説明

--- modal.js
+++ modal.js
@@ -6,5 +6,20 @@ open.addEventListener('click', function(){
     myModal.style.display = 'block';
 });
 
+//#myModal {
+//    display: none; /* 最初は隠しておく */
+//}
+
+// モーダルを出す流れ
+
+// JavaScriptが myModal というHTML要素を特定します。
+
+// myModal.style を通じて、その要素のCSSスタイルにアクセスします。
+
+// .display を指定することで、CSSの display プロパティをターゲットにします。
+
+// = 'block' とすることで、display プロパティの値を 'block' に「設定」します。
+
+
 
 
