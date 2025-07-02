88dd6d725ac0da2039f3404489cf35ba691d056c
hiroya0 <2000shigehiro0401@au.com>
2025-07-02 11:40:22 +0900

    ボタンを押してメッセージを出す

---
+++ modal.js
@@ -0,0 +1,7 @@
+const modalButton = document.getElementById('openModalBtn');
+modalButton.addEventListener('click',function(){
+    alert('ボタンがクリックされました');
+});
+
+//this.alertにしない
+//cssセレクタを参照するときは .をつける。idの場合は不要
