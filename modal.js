93cdbf36ab29b423c049700052d2b76b788a0f61
hiroya0 <2000shigehiro0401@au.com>
2025-07-03 11:32:22 +0900

    モーダル変更

--- modal.js
+++ modal.js
@@ -9,21 +9,3 @@ open.addEventListener('click', function(){
 close.addEventListener('click',function(){
     myModal.style.display = 'none';
 });
-
-//#myModal {
-//    display: none; /* 最初は隠しておく */
-//}
-
-// モーダルを出す流れ
-
-// JavaScriptが myModal というHTML要素を特定します。
-
-// myModal.style を通じて、その要素のCSSスタイルにアクセスします。
-
-// .display を指定することで、CSSの display プロパティをターゲットにします。
-
-// = 'block' とすることで、display プロパティの値を 'block' に「設定」します。
-
-
-
-
